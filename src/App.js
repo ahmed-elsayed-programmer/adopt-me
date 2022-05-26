9a341cd7ae051bf0e4831cdef33f313141aab1fb
Ahmed Elsayed <ahmedelsayed3157@gmail.com>
2022-05-26 09:45:12 +0200

    install tailwindcss and make some styles

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import ThemeContext from "./ThemeContext.js";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 
+import "./style.css";
+
 const App = () => {
   const theme = useState("blue");
 
@@ -12,15 +14,26 @@ const App = () => {
     <StrictMode>
       <ThemeContext.Provider value={theme}>
         <BrowserRouter>
-          <header>
-            <Link to={"/"}>
-              <h1>Adopt Me!</h1>
-            </Link>
-          </header>
-          <Routes>
-            <Route index element={<SearchParams />} />
-            <Route path="/details/:id" element={<Details />} />
-          </Routes>
+          <div
+            className="p-0 m-0"
+            style={{
+              background:
+                "url(http://pets-images.dev-apis.com/pets/wallpaperA.jpg)",
+            }}
+          >
+            <header className="w-full mb-10 text-center p-7 bg-gradient-to-b from-purple-400 via-pink-500 to-red-500">
+              <Link
+                to={"/"}
+                className="text-6xl hover:text-gray-200 text-white"
+              >
+                Adopt Me!
+              </Link>
+            </header>
+            <Routes>
+              <Route index element={<SearchParams />} />
+              <Route path="/details/:id" element={<Details />} />
+            </Routes>
+          </div>
         </BrowserRouter>
       </ThemeContext.Provider>
     </StrictMode>
